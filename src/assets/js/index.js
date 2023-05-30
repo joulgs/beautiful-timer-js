c6e9ebe88dbd2ebdb9e959b65675530931f25541
Jonathas Soares <joul.gs@gmail.com>
2023-05-30 17:34:24 -0300

    primeiro commit

---
+++ src/assets/js/index.js
@@ -0,0 +1,8 @@
+var sun = document.getElementById('sun');
+
+var btn_teste = document.getElementById('btn-teste');
+
+btn_teste.addEventListener('click', function () {
+    sun.style.transform = 'translateY(-100px)';
+});
+
